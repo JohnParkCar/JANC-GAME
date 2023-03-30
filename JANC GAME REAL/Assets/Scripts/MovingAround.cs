24963b9dc74319dce10314a1b06257b0f6ffbafb
JohnParkCar <jreaganp@gmail.com>
2023-03-30 12:30:30 -0500

    First Commit

---
+++ JANC GAME REAL/Assets/Scripts/MovingAround.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingAround : MonoBehaviour
+{
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        if (hor != 0 || vert != 0)
+        {
+            player.position += new Vector3(0.1f * hor, 0.1f * vert, 0);
+        }
+    }
+}
